02d161c76c0cca31140a9642fcf61c581268a172
V-arun03 <112625305+V-arun03@users.noreply.github.com>
2022-09-19 09:57:06 +0530

    Add files via upload

---
+++ agevar.py
@@ -0,0 +1,5 @@
+a = int(input("enter the age"));
+if (a>=18):
+      print("the user is eligible for voting")
+else:
+      print("the user is  not eligible for  voting")
---
+++ factVar.py
@@ -0,0 +1,9 @@
+number = int(input("enter the number"))
+factorial = 1
+if (number<0):
+      print("there is no factorial for negative  number")
+else:
+      for i in range (1,number+1):
+            factorial = factorial * i
+            print("",factorial)
+      
